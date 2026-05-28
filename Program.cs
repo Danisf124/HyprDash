ae81a8497f6ceb38e9e026508519f2b0b3a43c3e
Danisf124 <danasahno54@gmail.com>
2026-05-28 18:57:30 +0300

    First commtit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HyprDash
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            //Time and data
+
+            DateTime currentDateTime = DateTime.Now;
+
+            // Weather
+
+            // Todo List
+            
+            //output
+        }
+    }
+}
