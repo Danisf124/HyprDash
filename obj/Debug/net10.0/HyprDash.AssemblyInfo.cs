c57d29103de4dca7dea12b0385205ace9a50e5b2
Danisf124 <danasahno54@gmail.com>
2026-05-31 18:10:55 +0300

    Delete DatabaseContext.cs

--- obj/Debug/net10.0/HyprDash.AssemblyInfo.cs
+++ obj/Debug/net10.0/HyprDash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb19606e14b859de6c6b70e85a2d754de834c463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+688e134309d361b2b6cd873bc0cfcd3771dc541c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
