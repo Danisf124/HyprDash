af8e1f988e753414a2034042f37780c581ca59f0
Danisf124 <danasahno54@gmail.com>
2026-06-01 10:27:03 +0300

    Add Spectrate console

--- obj/Debug/net10.0/HyprDash.AssemblyInfo.cs
+++ obj/Debug/net10.0/HyprDash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52506a1d8b5cc2a7596c781e1c16abb986b5a0cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e70a11b6d81977e2f597a44f03bdd6e156e539b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HyprDash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
